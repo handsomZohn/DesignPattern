417dc2296d004b1252bce51a7700288ca39e04c5
handsomZohn <1457579742@qq.com>
2017-11-19 18:16:07 +0800

    设计模式

---
+++ celuemoshi/src/celuemoshi/duck/ModelDuck.java
@@ -0,0 +1,23 @@
+package celuemoshi.duck;
+
+import celuemoshi.fly.FlyNoWay;
+import celuemoshi.quack.MuteQuack;
+
+/**
+ * Created by zohn on 2017/11/10
+ * 新建一个模型鸭子
+ */
+public class ModelDuck extends Duck {
+
+    public ModelDuck() {
+        flyBehavior = new FlyNoWay(); //不会飞的模型鸭
+        quackBehavior = new MuteQuack();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I am a model duck!!!!");
+    }
+
+
+}
